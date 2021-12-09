3b9b4ad4ad86ea938e1615baa44c54c3d26d1716
FathurGans1 <93472221+FathurGans1@users.noreply.github.com>
2021-12-09 22:20:26 +0700

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_\nestimasi selesai ${groups.length * 1.5} detik`, m)
   for (let id of groups) {
     await delay(1500)
-    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 Stikerin Broadcast 〕\n\n' + teks + '\n\n' + '© stikerin'), true).catch(_ => _)
+    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 ☠𝕯𝖆𝖗𝖐 𝕬𝖓𝖌𝖊𝖑 Broadcast 〕\n\n' + teks + '\n\n' + '© 𝕯𝖆𝖗𝖐 𝕬𝖓𝖌𝖊𝖑'), true).catch(_ => _)
   }
   m.reply('_*Broadcast Selesai*_')
 }
