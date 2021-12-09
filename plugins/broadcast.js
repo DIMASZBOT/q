28b0e50b4bf9de38c12206bdf905a550ab5ff5b4
FathurGans1 <93472221+FathurGans1@users.noreply.github.com>
2021-12-09 22:23:06 +0700

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_\nestimasi selesai ${chats.length * 1.5} detik`, m)
   for (let id of chats) {
     await delay(1500)
-    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 Stikerin Broadcast 〕\n\n' + teks + '\n\n' + '© stikerin'), true).catch(_ => _)
+    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 ☠𝕯𝖆𝖗𝖐 𝕬𝖓𝖌𝖊𝖑 Broadcast 〕\n\n' + teks + '\n\n' + '© 𝕯𝖆𝖗𝖐 𝕬𝖓𝖌𝖊𝖑'), true).catch(_ => _)
   }
   m.reply('_*Broadcast Selesai*_')
 }
