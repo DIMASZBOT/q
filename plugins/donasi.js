428d349dfde4e033a48d0114e304afd48644991c
FathurGans1 <93472221+FathurGans1@users.noreply.github.com>
2021-12-09 22:25:09 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
-└────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
+ 〔 Donasi • Pulsa 〕
+☯ 089527393988
+☯ 08566732166
+
+`.trim(), '© 𝕯𝖆𝖗𝖐 𝕬𝖓𝖌𝖊𝖑', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
