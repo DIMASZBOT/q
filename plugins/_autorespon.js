9e20241d16e7eafe25329524e7314a8f886a5ac5
FathurSaha12 <95715048+FathurSaha12@users.noreply.github.com>
2021-12-12 03:54:19 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -14,8 +14,8 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? 'stikerin tidak aktif' : banned ? 'kamu dibanned' : 'stikerin disini',
-                '© stikerin',
+                isBanned ? 'Gw tidak aktif' : banned ? 'kamu dibanned' : 'Gw disini',
+                '© ☠𝕯𝖆𝖗𝖐 𝕬𝖓𝖌𝖊𝖑',
                 isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
                 isBanned ? '.unban' : banned ? '.owner' : '.?',
                 m.isGroup ? 'Ban' : isBanned ? 'Unban' : 'Donasi',
@@ -32,8 +32,7 @@ handler.all = async function (m, { isBlocked }) {
 ├ 30 Hari / Rp 10,000
 └────
 
-https://github.com/ariffb25/stikerinbot
-`.trim(), '© stikerin', 'Pemilik Bot', ',owner', m)
+`.trim(), '© ☠𝕯𝖆𝖗𝖐 𝕬𝖓𝖌𝖊𝖑', 'Pemilik Bot', ',owner', m)
     }
 
     // salam
@@ -63,7 +62,7 @@ https://github.com/ariffb25/stikerinbot
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | stikerinbot oleh ariffb`).catch(_ => _)
+        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | ☠𝕯𝖆𝖗𝖐 𝕬𝖓𝖌𝖊𝖑`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
